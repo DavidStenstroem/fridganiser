7f2a2589995431b1492c87f3eb3ee7052aa32418
DavidStenstroem <david@stenstroem.dk>
2020-09-24 20:34:55 +0200

    chore(prettier): run `prettier --write`

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,6 @@
 import React, { FunctionComponent } from 'react'
 import { AppProps } from 'next/app'
 
-const App: FunctionComponent<AppProps> = ({ Component, pageProps }) => (
-  <Component {...pageProps} />
-)
+const App: FunctionComponent<AppProps> = ({ Component, pageProps }) => <Component {...pageProps} />
 
 export default App
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,6 @@
 import React, { FunctionComponent } from 'react'
 import Head from 'next/head'
 
-const Index: FunctionComponent = () => (
-  <div>Index</div>
-)
+const Index: FunctionComponent = () => <div>Index</div>
 
 export default Index
